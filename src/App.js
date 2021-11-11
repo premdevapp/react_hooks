7435e9b018fbcb3f777a6304637edbfb3e11a818
premnath <premdevapp@gmail.com>
2021-11-11 13:01:03 +0530

    useref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
+
 import "./App.css"
+
 const App = () => {
-const [counter, setCounter] = useState(0);
+  const [state, setState] = useState(undefined);
+  const inputRef = useRef(null);
+  const handleClick = () => {
+    inputRef.current.focus();
+    setState(inputRef.current.value);
+    inputRef.current.value = "";
+  }
   return (
     <div className="App">
-      <p>{counter}</p>
-      <button onClick={() => setCounter((prev)=>prev + 1)}>+</button>
+      <p>{state ?? "hey"}</p>
+      <input type="text" placeholder="type here to show" ref={inputRef} />
+      <button onClick={handleClick}>+</button>
     </div>
   )
 }
